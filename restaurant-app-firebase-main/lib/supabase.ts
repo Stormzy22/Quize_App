4a4acba1d7768132ee172998aa1e46efcfccdcd3
Charaf <charaf.inaoui@outlook.com>
2025-12-12 13:42:01 +0000

    Switching to desktop

--- restaurant-app-firebase-main/lib/supabase.ts
+++ restaurant-app-firebase-main/lib/supabase.ts
@@ -1,28 +1,31 @@
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AppState } from "react-native";
-import { Database } from "@/database.types";
+import { Database } from "database.types";
 
-const supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+// ❗ Vul hier gewoon je eigen URL en key in
+const supabaseURL = "https://ihdxlnxubcwwegwlsbxj.supabase.co";
+const supabasePublishableKey =
+  "sb_publishable_AncbMAm...zXiAmmpx"; // volledige key plakken
 
+// Deze check mag je weglaten of zo laten staan, hij faalt nu toch niet meer
 if (!supabaseURL || !supabasePublishableKey) {
-    throw new Error("Missing supabase environment variables");
+  throw new Error("Missing supabase environment variables");
 }
 
 export const supabase = createClient<Database>(supabaseURL, supabasePublishableKey, {
-    auth: {
-        storage: AsyncStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: false
-    }
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
 });
 
 AppState.addEventListener("change", (state) => {
-    if (state === "active") {
-        supabase.auth.startAutoRefresh();
-    } else {
-        supabase.auth.stopAutoRefresh();
-    }
-})
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
